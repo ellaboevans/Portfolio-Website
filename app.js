a59a85b87a21768bea64ef09c30c44b65d8dfa0d
Evans Elabo <ellaboevans@gmail.com>
2022-11-06 14:27:49 +0000

    Update Preloader function
    Add auto type text to preloader text

--- app.js
+++ app.js
@@ -27,7 +27,25 @@ navLinks.forEach(function (link) {
 // preloader
 
 const preloader = document.querySelector(".preloader");
+const autoTyping = document.querySelector(".auto-type");
+let text = "Loading...";
+let i = 0;
 
-window.addEventListener("load", function () {
-  preloader.classList.add("hide-preloader");
-});
+function writeText() {
+  autoTyping.innerText = text.slice(0, i);
+  i++;
+  if (i > text.length) {
+    i = 0;
+  }
+}
+
+setInterval(writeText, 100);
+
+function myPreloader() {
+  window.addEventListener("load", function () {
+    preloader.classList.add("hide-preloader");
+  });
+}
+
+setTimeout(myPreloader, 10);
+clearTimeout(myPreloader, 1000);
