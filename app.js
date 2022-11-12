284ffb1e68e4579164596ba66b5b255e588f888e
Evans Elabo <ellaboevans@gmail.com>
2022-11-12 12:09:30 +0000

    Fix Greet Logic

--- app.js
+++ app.js
@@ -327,7 +327,7 @@ let hour = day.getHours();
 function greet() {
   if (hour < 12) {
     return "Good Morning";
-  } else if (hour > 12 && hour < 17) {
+  } else if (hour >= 12 && hour < 17) {
     return "Good afternoon";
   } else return "Good evening";
 }
